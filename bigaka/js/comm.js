976d6b352757bc85068e7acfe4ac5ea929ddb662
HULIANGA <835710406@qq.com>
2017-01-16 11:18:14 +0800

    会员概况更新

--- bigaka/js/comm.js
+++ bigaka/js/comm.js
@@ -1,7 +1,39 @@
 var loadingOption = {
-  text: '',
-  color: '#1ABB9C',
-  textColor: '#000',
-  maskColor: 'rgba(255, 255, 255, 0.8)',
-  zlevel: 0
+	text: '',
+	color: '#1ABB9C',
+	textColor: '#000',
+	maskColor: 'rgba(255, 255, 255, 0.8)',
+	zlevel: 0
 }
+var dateRangeOption = {
+	endDate: moment(),
+	minDate: '01/01/2000',
+	maxDate: new Date(),
+	showDropdowns: true,
+	timePicker: false,
+	timePickerIncrement: 1,
+	timePicker12Hour: true,
+	opens: 'left',
+	buttonClasses: ['btn btn-default'],
+	applyClass: 'btn-small btn-success',
+	cancelClass: 'btn-small',
+	format: 'YYYY.MM.DD',
+	separator: ' - ',
+	locale: {
+      applyLabel: '确定',
+      cancelLabel: '取消',
+      fromLabel: '开始',
+      toLabel: '截至',
+      customRangeLabel: '自定义',
+      daysOfWeek: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+      monthNames: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+      firstDay: 1
+    }
+  };
+
+$(function(){
+	$('body').on('click', '.chart-tabs span', function(){
+		$(this).parents('.chart-tabs').find('span').removeClass('active');
+		$(this).addClass('active');
+	})
+})
--- bigaka/js/user-subdivision.js
+++ bigaka/js/user-subdivision.js
@@ -1,3 +1,8 @@
+function resetCharts(){
+	$.each(pageEcharts, function(index){
+		this.resize();
+	})
+}
 $(function(){
 	// 标题tips
 	var userStatisticsTipsContent = 
@@ -8,4 +13,156 @@ $(function(){
 		placement: 'bottom',
 		trigger: 'hover'
 	})
+	// 会员标签分布tips
+	var userTagTipsContent = 
+		'会员标签分布，展示各标签组累计会员人数，包括手机会员及微信会员。由于1个会员可能存在多个标签，因此所有标签会员数累计可能大于总累计会员数。'
+	$('#userTagTips').popover({
+		content: userTagTipsContent,
+		html: true,
+		placement: 'bottom',
+		trigger: 'hover'
+	})
+	window.pageEcharts = []; // 记录页面上所有的echart，方便统一resize
+	$('#menu_toggle').on('click', function() {
+		resetCharts();
+	});
+	$(window).resize(function(){
+		resetCharts();
+	});
+	
+//	会员标签***************************************************************************************************
+	var userTagChart = echarts.init(document.getElementById('userTag')); // 基于准备好的dom，初始化echarts实例
+	userTagChart.showLoading('default', loadingOption);
+	pageEcharts.push(userTagChart);
+	userTagChart.setOption({
+		color : [ '#1ABB9C', '#7bd9a5' ],
+		title : { // 标题
+			show : false
+		},
+		tooltip : { // 鼠标悬浮提示框
+			position: 'top'
+		},
+		legend : { // 图例
+			show : true,
+			left : 0,
+			top: 0,
+			data:[{
+				name: '手机会员'
+			},{
+				name: '微信会员'
+			}]
+		},
+		grid : { // 网格
+			left: '1%',
+			top: 40,
+      right: 20,
+      bottom: 20,
+      containLabel: true
+		},
+		xAxis : {
+			type : 'value',
+			splitLine: {
+				lineStyle:{
+					type: 'dashed'
+				}
+			},
+			axisLine: {
+				lineStyle:{
+					color: '#888888'
+				}
+			},
+			axisLabel: {
+				
+			}
+		},
+		yAxis : {
+			type : 'category',
+			splitLine: {
+				lineStyle:{
+					type: 'dashed'
+				}
+			},
+			axisLine: {
+				lineStyle:{
+					color: '#888888'
+				}
+			},
+			data: []
+		},
+		series : [{
+			name: '手机会员',
+			type: 'bar',
+			stack: '全部会员',
+			label: {
+				normal: {
+					show: true,
+					position: 'insideRight'
+				}
+			}
+		},{
+			name: '微信会员',
+			type: 'bar',
+			stack: '全部会员',
+			label: {
+				normal: {
+					show: true,
+					position: 'insideRight'
+				}
+			}
+		}]
+	}); // 生效配置
+	// 设置会员标签数据
+	var userTagPhoneData = [{
+		value: 1438,
+		name: '标签一'
+	},{
+		value: 866,
+		name: '标签二'
+	},{
+		value: 1048,
+		name: '标签三'
+	},{
+		value: 566,
+		name: '标签四'
+	},{
+		value: 1255,
+		name: '标签五'
+	},{
+		value: 788,
+		name: '标签六'
+	}];
+	var userTagWchatData = [{
+		value: 866,
+		name: '标签一'
+	},{
+		value: 566,
+		name: '标签二'
+	},{
+		value: 1255,
+		name: '标签三'
+	},{
+		value: 788,
+		name: '标签四'
+	},{
+		value: 1438,
+		name: '标签五'
+	},{
+		value: 1048,
+		name: '标签六'
+	}];
+	setTimeout(function(){
+		userTagChart.hideLoading();
+		userTagChart.setOption({
+			yAxis : {
+				data : userTagWchatData.map(function(item) {
+					return item.name
+				})
+			},
+			series : [{
+				data : userTagPhoneData
+			},{
+				data: userTagWchatData
+			}]
+		});
+	}, 1000)
 })
