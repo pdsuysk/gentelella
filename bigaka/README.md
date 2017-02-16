347fb3b10da4a3a8ba48eed76770a0e7a1c762bb
HULIANGA <835710406@qq.com>
2017-02-16 17:51:05 +0800

    完善商品统计页面

---
+++ bigaka/README.md
@@ -0,0 +1,12 @@
+// 运行命令
+npm install
+gulp
+
+gulpfile.js 配置了proxy，ajax可以跨域请求
+
+// 全局公共变量
+1、ctx：ajax请求根目录
+2、storeId：商户id
+3、getUrlParam()：获取url指定参数值
+4、loadingOption：echart loading样式配置
+5、dateRangeOption：日期选择控件配置
--- bigaka/js/sales-goods.js
+++ bigaka/js/sales-goods.js
@@ -15,55 +15,55 @@ $(function() {
 		resetCharts();
 	});
 
-  // 所有门店数据
+	// 初始化商品top10chart
 	window.goodsTop10StatisticsChart = echarts.init(document.getElementById('goodsTop10Statistics'));
 	pageEcharts.push(goodsTop10StatisticsChart);
 	goodsTop10StatisticsChart.showLoading('default', loadingOption);
-	var goodsTop10StatisticsColor = [ '#1ABB9C', '#7bd9a5', '#3fb1e3' ];
+	var goodsTop10StatisticsColor = ['#1ABB9C', '#7bd9a5', '#3fb1e3'];
 	goodsTop10StatisticsChart.setOption({
-		color : goodsTop10StatisticsColor,
-		title : {
-			show : false
+		color: goodsTop10StatisticsColor,
+		title: {
+			show: false
 		},
-		legend : {
-			data : [ '销量', '销售额' ],
-			selected : {
-				'销量' : true,
-				'销售额' : false
+		legend: {
+			data: ['销量', '销售额'],
+			selected: {
+				'销量': true,
+				'销售额': false
 			},
-			selectedMode : 'single',
-			right : 10
+			selectedMode: 'single',
+			right: 10
 		},
-		tooltip : {
-			trigger : 'axis',
-			axisPointer : {
-				type : 'shadow'
+		tooltip: {
+			trigger: 'axis',
+			axisPointer: {
+				type: 'shadow'
 			}
 		},
-		grid : { // 网格
-			left : 25,
-			top : 40,
-			right : 30,
-			bottom : 20,
-			containLabel : true
+		grid: { // 网格
+			left: 25,
+			top: 40,
+			right: 30,
+			bottom: 20,
+			containLabel: true
 		},
-		xAxis : {
+		xAxis: {
 			type: 'value',
-			splitLine : {
-				lineStyle : {
-					type : 'dashed'
+			splitLine: {
+				lineStyle: {
+					type: 'dashed'
 				}
 			},
-			axisLine : {
-				lineStyle : {
-					color : '#888888'
+			axisLine: {
+				lineStyle: {
+					color: '#888888'
 				}
 			},
-			axisTick : {
-				alignWithLabel : true
+			axisTick: {
+				alignWithLabel: true
 			}
 		},
-		yAxis : {
+		yAxis: {
 			name: 'Top10',
 			nameLocation: 'start',
 			nameTextStyle: {
@@ -82,129 +82,68 @@ $(function() {
 				show: false
 			},
 			z: 2,
-			splitLine : {
-				lineStyle : {
-					type : 'dashed'
+			splitLine: {
+				lineStyle: {
+					type: 'dashed'
 				}
 			},
-			axisLine : {
-				lineStyle : {
-					color : '#888888'
+			axisLine: {
+				lineStyle: {
+					color: '#888888'
 				}
 			},
-			data : []
+			data: []
 		},
-		series : [ {
-			name : '销量',
-			type : 'bar',
-			legendHoverLink : false
+		series: [{
+			name: '销量',
+			type: 'bar',
+			legendHoverLink: false
 		}, {
-			name : '销售额',
-			type : 'bar',
-			legendHoverLink : false
+			name: '销售额',
+			type: 'bar',
+			legendHoverLink: false
 		}]
 	});
-
-	curStartDate = moment().subtract(6, 'days').format('YYYY-MM-DD');
-	curEndDate = moment().format('YYYY-MM-DD');
-	getTop10Goods();
-	$('#everyDateRange').daterangepicker(dateRangeOption);
-	$('#everyDateRange').on('apply.daterangepicker', function(ev, picker) {
-		$('#everyDateRange span').html(picker.startDate.format('YYYY.MM.DD') + ' - ' + picker.endDate.format('YYYY.MM.DD'));
-		$('#everyDateChange span').removeClass('active');
-		curStartDate = picker.startDate.format('YYYY-MM-DD');
-		curEndDate = picker.endDate.format('YYYY-MM-DD');
-		getTop10Goods();
-	});
-	$('#everyDateChange span').click(function(){
-		$('#everyDateRange span').html('点击选择日期');
-		var dateRange = parseInt($(this).attr('data-value'));
-		curStartDate = moment().subtract(dateRange-1, 'days').format('YYYY-MM-DD');
-		curEndDate = moment().format('YYYY-MM-DD')
-		getTop10Goods();
-	})
-	// 所有门店数据
-	function getTop10Goods (startDateFormat, endDateFormat) {
-		if(!startDateFormat){
+	// 获取商品top10数据
+	function getTop10Goods(startDateFormat, endDateFormat) {
+		if(!startDateFormat) {
 			startDateFormat = curStartDate
 		}
-		if(!endDateFormat){
+		if(!endDateFormat) {
 			endDateFormat = curEndDate
 		}
 		goodsTop10StatisticsChart.showLoading('default', loadingOption);
 		$.ajax({
-			url: ctx + 'ProductOrderAction /getProductTotalList.do?parentStoreId=' + storeId + '&startDate=' + startDateFormat + '&endDate=' + endDateFormat + '&orderBy=' + top10OrderBy + '&limitSize=10',
+			url: ctx + 'ProductOrderAction/getProductTotalList.do?parentStoreId=' + storeId + '&startDate=' + startDateFormat + '&endDate=' + endDateFormat + '&orderBy=' + top10OrderBy + '&limitSize=10',
 			type: 'post',
-			success: function (response) {
-				if (response.code === 0) {
+			success: function(response) {
+				if(response.code === 0) {
 					var seriesData = [
-						response.data.map(function(item){
+						response.data.map(function(item) {
 							return item.productSaleNumber
 						}),
-						response.data.map(function(item){
-							return (item.productSaleAmount / 100).toFixed(2)
-					})]
+						response.data.map(function(item) {
+							return(item.productSaleAmount / 100).toFixed(2)
+						})
+					]
 					goodsTop10StatisticsChart.hideLoading();
 					goodsTop10StatisticsChart.setOption({
 						yAxis: {
-							data: response.data.map(function(item){
+							data: response.data.map(function(item) {
 								return item.productName
 							})
 						},
 						series: [{
 							data: seriesData[0]
-						},{
+						}, {
 							data: seriesData[1]
 						}]
 					});
-//					goodsTop10StatisticsChart.off('click');
-//					goodsTop10StatisticsChart.on('click', function(event) { // 选中一个门店-------------------------------------
-//						if (event.componentType === 'series') {
-//							var newSeriesData = $.extend(true, {}, seriesData);
-//							newSeriesData[event.seriesIndex][event.dataIndex].label = {
-//								normal : {
-//									show : true,
-//									position : 'top',
-//									textStyle : {
-//										color : '#E74C3C'
-//									}
-//								},
-//								emphasis : {
-//									show : true,
-//									position : 'top',
-//									textStyle : {
-//										color : '#E74C3C'
-//									}
-//								}
-//							};
-//							newSeriesData[event.seriesIndex][event.dataIndex].itemStyle = {
-//								normal : {
-//									color : '#E74C3C'
-//								},
-//								emphasis : {
-//									color : '#E74C3C'
-//								}
-//							}
-//
-//							goodsTop10StatisticsChart.setOption({
-//								series: [{
-//									data:	newSeriesData[0]
-//								},{
-//									data: newSeriesData[1]
-//								},{
-//									data: newSeriesData[2]
-//								}]
-//							});
-//							$('#selectedStoreName').text(newSeriesData[event.seriesIndex][event.dataIndex].name)
-//							getTotalOrderData(moment().subtract(6, 'days').format('YYYY-MM-DD'), moment().format('YYYY-MM-DD'), newSeriesData[event.seriesIndex][event.dataIndex].id);
-//							getEveryOrderData(moment().subtract(6, 'days').format('YYYY-MM-DD'), moment().format('YYYY-MM-DD'), newSeriesData[event.seriesIndex][event.dataIndex].id);
-//						}
-//					});
 					goodsTop10StatisticsChart.off('legendselectchanged');
 					goodsTop10StatisticsChart.on('legendselectchanged', function(event) {
-						if (event.name === '销量') {
+						if(event.name === '销量') {
 							top10OrderBy = 'productSaleNumber'; // productSaleNumber:销量；productSaleAmount:销售金额；
-						} else if (event.name === '销售额') {
+						} else if(event.name === '销售额') {
 							top10OrderBy = 'productSaleAmount'; // productSaleNumber:销量；productSaleAmount:销售金额；
 						}
 						getTop10Goods();
@@ -213,164 +152,181 @@ $(function() {
 			}
 		})
 	}
-	// 门店详情
-//	var oneShopStatisticsChart = echarts.init(document.getElementById('oneShopStatistics'));
-//	pageEcharts.push(oneShopStatisticsChart);
-//	// oneShopStatisticsChart.showLoading('default', loadingOption);
-//	oneShopStatisticsColor = [ '#1ABB9C', '#7bd9a5', '#3fb1e3' ];
-//	oneShopStatisticsChart.setOption({
-//  color: oneShopStatisticsColor,
-//  tooltip: {
-//    trigger: 'axis'
-//  },
-//  grid: {
-//    left: 0,
-//			bottom: 20,
-//			containLabel : true
-//  },
-//  legend: {
-//    data: ['付款订单', '付款金额', '单均价']
-//  },
-//  xAxis: [{
-//    type: 'category',
-//			axisLine: {
-//				lineStyle: {
-//					color: '#888'
-//				}
-//			},
-//    axisTick: {
-//      alignWithLabel: true
-//    },
-//    data: []
-//  }],
-//  yAxis: [{
-//      type: 'value',
-//      name: '付款订单',
-//      // min: 0,
-//      // max: 250,
-//      position: 'right',
-//      axisLine: {
-//        lineStyle: {
-//          color: oneShopStatisticsColor[0]
-//        }
-//      },
-//				splitLine : {
-//					show: false,
-//					lineStyle : {
-//						type : 'dashed'
-//					}
-//				},
-//      axisLabel: {
-//        formatter: '{value}'
-//      }
-//    },
-//    {
-//      type: 'value',
-//      name: '付款金额',
-//      // min: 0,
-//      // max: 250,
-//      position: 'right',
-//      offset: 80,
-//      axisLine: {
-//        lineStyle: {
-//          color: oneShopStatisticsColor[1]
-//        }
-//      },
-//				splitLine : {
-//					show: false,
-//					lineStyle : {
-//						type : 'dashed'
-//					}
-//				},
-//      axisLabel: {
-//        formatter: '{value}'
-//      }
-//    },
-//    {
-//      type: 'value',
-//      name: '单均价',
-//      // min: 0,
-//      // max: 25,
-//      position: 'left',
-//      axisLine: {
-//        lineStyle: {
-//          color: oneShopStatisticsColor[2]
-//        }
-//      },
-//				splitLine : {
-//					lineStyle : {
-//						type : 'dashed'
-//					}
-//				},
-//      axisLabel: {
-//        formatter: '{value}'
-//      }
-//    }
-//  ],
-//  series: []
-//});
-//
-//	// 总订单数和销售额
-//	function getTotalOrderData (startDateFormat, endDateFormat, subStoreId) {
-//		if(!subStoreId){
-//			subStoreId = storeId;
-//		}
-//		$.ajax({
-//			url: ctx + 'OrderStoreDateAction/getOSDTotalByStoreId.do?storeId=' + subStoreId + '&startDate=' + startDateFormat + '&endDate=' + endDateFormat,
-//			type: 'post',
-//			success: function (response) {
-//				if (response.code === 0) {
-//					$('#totalOrderNum').text(response.data.orderPaidNumberTotal);
-//					$('#totalOrderPrice').text((response.data.orderPaidTotal / 100).toFixed(2));
-//					$('#everyOrderPrice').text((response.data.orderPaidTotal / response.data.orderPaidNumberTotal / 100).toFixed(2))
-//				}
-//			}
-//		})
-//	}
-//	// 每日订单数和销售额
-//	function getEveryOrderData (startDateFormat, endDateFormat, subStoreId) {
-//		oneShopStatisticsChart.showLoading('default', loadingOption);
-//		if(!subStoreId){
-//			subStoreId = storeId;
-//		}
-//		$.ajax({
-//			url: ctx + 'OrderStoreDateAction/getSubStoreTotalListByDate.do?storeId=' + subStoreId + '&startDate=' + startDateFormat + '&endDate=' + endDateFormat,
-//			type: 'post',
-//			success: function (response) {
-//				if (response.code === 0) {
-//					oneShopStatisticsChart.hideLoading();
-//					oneShopStatisticsChart.setOption({
-//						xAxis: [{
-//				      data: response.data.map(function(item){
-//								return item.dateTime
-//							})
-//				    }],
-//						series : [{
-//				      name: '付款订单',
-//				      type: 'bar',
-//							data : response.data.map(function(item){
-//								return item.orderPaidNumberTotal
-//							})
-//				    },
-//				    {
-//				      name: '付款金额',
-//				      type: 'bar',
-//				      yAxisIndex: 1,
-//							data : response.data.map(function(item){
-//								return (item.orderPaidTotal / 100).toFixed(2)
-//							})
-//				    },
-//				    {
-//				      name: '单均价',
-//				      type: 'line',
-//				      yAxisIndex: 2,
-//							data : response.data.map(function(item){
-//								return (item.orderPaidTotal / item.orderPaidNumberTotal / 100).toFixed(2)
-//							})
-//				    }]
-//					})
-//				}
-//			}
-//		})
-//	}
+
+	// 点击表格的一行查询对应商品销售数据
+	$('#productTable tbody').on('click', 'tr', function() {
+		$('#productTable tbody tr.success').removeClass('success');
+		$(this).addClass('success');
+		$('#activeGoodsName').text($(this).find('.product-name').text());
+		getGoodsTrend($(this).find('.product-name').attr('data-id'));
+	})
+	// 获取所有商品数据
+	function getAllGoods(startDateFormat, endDateFormat) {
+		if(!startDateFormat) {
+			startDateFormat = curStartDate
+		}
+		if(!endDateFormat) {
+			endDateFormat = curEndDate
+		}
+		$.ajax({
+			url: ctx + 'ProductOrderAction/getProductTotalList.do?parentStoreId=' + storeId + '&startDate=' + startDateFormat + '&endDate=' + endDateFormat + '&orderBy=' + top10OrderBy + '&limitSize=',
+			type: 'post',
+			success: function(response) {
+				if(response.code === 0) {
+					$('#productTable tbody').html('');
+					$.each(response.data, function(index, product) {
+						var trHtml =
+							'<tr>' +
+							'<td class="product-name" data-id="' + product.productId + '">' + product.productName + '</td>' +
+							'<td>' + product.productId + '</td>' +
+							'<td>' + product.productSaleNumber + '</td>' +
+							'<td>' + (product.productSaleAmount / 100).toFixed(2) + '</td>' +
+							'<td></td>' +
+							'<td></td>' +
+							'</tr>'
+						$('#productTable tbody').append(trHtml);
+					});
+					$('#productTable').DataTable();
+				}
+			}
+		})
+	}
+
+	// 单个商品销售趋势chart初始化
+	window.goodsTrendStatisticsChart = echarts.init(document.getElementById('goodsTrendStatistics'));
+	pageEcharts.push(goodsTrendStatisticsChart);
+	goodsTrendStatisticsChart.showLoading('default', loadingOption);
+	var goodsTrendStatisticsColor = ['#1ABB9C', '#7bd9a5', '#3fb1e3'];
+	goodsTrendStatisticsChart.setOption({
+		color: goodsTrendStatisticsColor,
+		title: {
+			show: false
+		},
+		legend: {
+			data: ['销量', '销售额'],
+			selected: {
+				'销量': true,
+				'销售额': false
+			},
+			selectedMode: 'single',
+			left: '1%'
+		},
+		tooltip: {
+			trigger: 'axis',
+			axisPointer: {
+				type: 'shadow'
+			}
+		},
+		grid: { // 网格
+			left: '1%',
+			top: 40,
+			right: 30,
+			bottom: 40,
+			containLabel: true
+		},
+		xAxis: {
+			splitLine: {
+				lineStyle: {
+					type: 'dashed'
+				}
+			},
+			axisLine: {
+				lineStyle: {
+					color: '#888888'
+				}
+			},
+			axisTick : {
+				alignWithLabel : true
+			},
+			data: []
+		},
+		yAxis: {
+			splitLine: {
+				lineStyle: {
+					type: 'dashed'
+				}
+			},
+			axisLine: {
+				lineStyle: {
+					color: '#888888'
+				}
+			}
+		},
+		series: [{
+			name: '销量',
+			type: 'bar',
+			legendHoverLink: false
+		}, {
+			name: '销售额',
+			type: 'bar',
+			legendHoverLink: false
+		}]
+	});
+	// 切换按日、周、月统计
+	$('body').on('click', '.chart-tabs#goodTrendType span', function(){
+		getGoodsTrend($('#productTable tbody tr.success').find('.product-name').attr('data-id'))
+	})
+	// 查询某一商品的销售数据
+	function getGoodsTrend (productId, startDateFormat, endDateFormat) {
+		if(!startDateFormat) {
+			startDateFormat = curStartDate
+		}
+		if(!endDateFormat) {
+			endDateFormat = curEndDate
+		}
+		goodsTrendStatisticsChart.showLoading('default', loadingOption);
+		$.ajax({
+			url: ctx + 'ProductOrderAction/getProductTotalListByDate.do?productId=' + productId + '&startDate=' + startDateFormat + '&endDate=' + endDateFormat + '&dataType=' + $('#goodTrendType span.active').attr('data-value'),
+			type: 'post',
+			success: function(response) {
+				if(response.code === 0) {
+					var seriesData = [
+						response.data.map(function(item) {
+							return item.productAsleNumber
+						}),
+						response.data.map(function(item) {
+							return(item.productSaleAmount / 100).toFixed(2)
+						})
+					]
+					goodsTrendStatisticsChart.hideLoading();
+					goodsTrendStatisticsChart.setOption({
+						xAxis: {
+							data: response.data.map(function(item) {
+								return item.dateTime
+							})
+						},
+						series: [{
+							data: seriesData[0]
+						}, {
+							data: seriesData[1]
+						}]
+					});
+				}
+			}
+		})
+	}
+
+	curStartDate = moment().subtract(6, 'days').format('YYYY-MM-DD');
+	curEndDate = moment().format('YYYY-MM-DD');
+	// 初始化数据
+	getTop10Goods();
+	getAllGoods();
+	// 选择时间段
+	$('#everyDateRange').daterangepicker(dateRangeOption);
+	$('#everyDateRange').on('apply.daterangepicker', function(ev, picker) {
+		$('#everyDateRange span').html(picker.startDate.format('YYYY.MM.DD') + ' - ' + picker.endDate.format('YYYY.MM.DD'));
+		$('#everyDateChange span').removeClass('active');
+		curStartDate = picker.startDate.format('YYYY-MM-DD');
+		curEndDate = picker.endDate.format('YYYY-MM-DD');
+		getTop10Goods();
+		getAllGoods();
+	});
+	$('#everyDateChange span').click(function() {
+		$('#everyDateRange span').html('点击选择日期');
+		var dateRange = parseInt($(this).attr('data-value'));
+		curStartDate = moment().subtract(dateRange - 1, 'days').format('YYYY-MM-DD');
+		curEndDate = moment().format('YYYY-MM-DD')
+		getTop10Goods();
+		getAllGoods();
+	})
 })
